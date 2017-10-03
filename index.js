38579fb43155b0c2f75e96287f1f9980fb1ab09d
andangrd <andangrian.dimas@mitrais.com>
2017-10-04 00:52:19 +0700

    create package json

---
+++ index.js
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+import { View } from 'react-native';
+
+class Markdown extends Component {
+  render() {
+    return <View />;
+  }
+}
+
+export default Markdown;
